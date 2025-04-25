fdab878a3dd8b4813f22fed28f2b2db4132226a5
Lucas Santos <lukisantsuk@gmail.com>
2025-04-25 22:09:34 +0100

    adding initial page and result page to a function

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,6 +3,9 @@
 //Se o usuário desejar que o número possa ser repetido, bem, caso contrário nao permita que o número se repita.
 
 let form = document.querySelector("form")
+const initialPage = document.getElementById("initialPage")
+const resultPage = document.getElementById("resultPage")
+let retry = document.getElementById("drawAgain")
 
 function drawNumber() {
   let numbers = document.getElementById("numbers")
@@ -16,7 +19,8 @@ function drawNumber() {
 
   if (numbers > 0 && min <= max) {
     if (checkbox.checked) {
-      if (numbers <= max) {
+      if (numbers <= max - min + 1) {
+        showResult()
         for (let i = 1; i <= numbers; i++) {
           let num = Math.floor(Math.random() * (max - min + 1)) + min
           while (randomNumbers.includes(num)) {
@@ -27,10 +31,11 @@ function drawNumber() {
         }
       } else {
         alert(
-          "A quantidade de números desejada a serem sorteados deve ser menor ou igual ao número máximo/final do intervalo."
+          "A quantidade de números desejada a serem sorteados deve ser menor ou igual ao fim - inicio + 1."
         )
       }
     } else {
+      showResult()
       for (let i = 1; i <= numbers; i++) {
         let num = Math.floor(Math.random() * (max - min + 1)) + min
         randomNumbers.push(num)
@@ -44,7 +49,22 @@ function drawNumber() {
   }
 }
 
+function showResult() {
+  initialPage.style.display = "none"
+  resultPage.style.display = "block"
+}
+
+function showInitial() {
+  resultPage.style.display = "none"
+  initialPage.style.display = "block"
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault()
   drawNumber()
+})
+
+retry.addEventListener("click", (event) => {
+  event.preventDefault()
+  showInitial()
 })
